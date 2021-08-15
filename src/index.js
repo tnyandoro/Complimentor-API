648f493359427c6beb45ef4adc33b90b80bae788
Tendai <tendai1@hotmail.com>
2021-08-15 22:49:55 +0200

    Set get api data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ document.getElementById('getText').addEventListener
 ('click', getText);
 document.getElementById('getUsers').addEventListener
 ('click', getUsers);
+document.getElementById('getPosts').addEventListener
+('click', getPosts);
 
 function getText(){
   fetch('sample.txt')
@@ -22,9 +24,30 @@ function getUsers() {
     let output = `<h2>Users</h2>`;
     data.forEach((user) => {
       output += `
-      <ul>ID:${user.id}</ul>
-      <ul>Name:${user.name}</ul>
-      <ul>Email:${user.email}</ul>
+      <ul>
+      <li>ID:${user.id}</li>
+      <li>Name:${user.name}</li>
+      <li>Email:${user.email}</li>
+      </ul>
+      `;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+}
+
+function getPosts() {
+  fetch('posts.json')
+  .then((res)=> res.json())
+  .then((data)=> {
+    let output = `<h2>Posts</h2>`;
+    data.forEach((post) => {
+      output += `
+      <ul>
+      <li>ID:${post.id}</li>
+      <li>Title:${post.title}</li>
+      <li>Body:${post.body}</li>
+      <li>User ID:${post.userId}</li>
+      </ul>
       `;
     });
     document.getElementById('output').innerHTML = output;
