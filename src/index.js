0f613ecb20a8f7098a9578dac0befc158015188a
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-15 22:28:00 +0200

    Set get users

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@
 
 document.getElementById('getText').addEventListener
 ('click', getText);
+document.getElementById('getUsers').addEventListener
+('click', getUsers);
 
 function getText(){
   fetch('sample.txt')
   .then((res)=> res.text())
   .then((data) => {
     document.getElementById('output').innerHTML = data;
+  })
+  .catch((err) => console.log(err))
+}
+
+function getUsers() {
+  fetch('users.json')
+  .then((res)=> res.json())
+  .then((data)=> {
+    let output = `<h2>Users</h2>`;
+    console.log(data);
   })
 }
